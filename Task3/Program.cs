d94d28b6572811b5163e64a21309d9c1e5a7f2d8
den_minus <den.minus@mail.ru>
2023-01-24 19:02:20 +0300

    дни недели по по номеру дня

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите номер дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n==1)
+{
+    Console.WriteLine("Первый день недели понедельник");
+}
+else if(n==2)
+{
+    Console.WriteLine("Второй день недели вторник");
+}
+else if(n==3)
+{
+    Console.WriteLine("Третий день недели среда");
+}
+else if(n==4)
+{
+    Console.WriteLine("Четвёртый день недели четверг");
+}
+else if(n==5)
+{
+    Console.WriteLine("Пятый день недели пятница");
+}
+else if(n==6)
+{
+    Console.WriteLine("Шестой день недели суббота");
+}
+else if(n==7)
+{
+    Console.WriteLine("Судьмой день недели Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Дней с таким числом нет!");
+}
