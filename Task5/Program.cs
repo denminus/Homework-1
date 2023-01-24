2747ad6aadd90159d29a4f9e0e73be59df32adb1
den_minus <den.minus@mail.ru>
2023-01-24 20:04:38 +0300

    Определение чётности числа

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int n=a%2;
+
+if (n==0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число не чётное");
+}
