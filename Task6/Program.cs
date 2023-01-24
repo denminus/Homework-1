97c370be56c613ce99823ca75fadc5b16b88591a
den_minus <den.minus@mail.ru>
2023-01-24 20:42:56 +0300

    чётные числа от 1 до N

---
+++ Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите положительное целое число, больше 1 ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count= 2;
+if (n > 1)
+{
+    int N=n-n%2;
+    while (count<=N)
+    {
+        Console.WriteLine(count);
+        count=count+2;
+    }   
+    
+}
+else 
+{
+    Console.WriteLine("Число ,должно быть больше 1 ");
+
+}
