5771c633b2c435ffce36a8b12b0d8b3ae364feac
den_minus <den.minus@mail.ru>
2023-01-24 22:00:20 +0300

    Наибольшая цифра числа

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число на отрезке [10,99]");
+int n = Convert.ToInt32(Console.ReadLine());
+int number1 = n / 10;
+int number2 = n % 10;
+int max=0;
+if (number1 > number2)
+{
+    max = number1;
+}
+else {
+    max = number2;
+}
+
+Console.Write("Наибольшая цифра числа ");
+Console.WriteLine(max);
