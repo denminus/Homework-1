760d675e35484dd5ba0ae0220c91c971c12afd5d
den_minus <den.minus@mail.ru>
2023-01-24 19:47:51 +0300

    Максимальное число из трёх

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double max=a;
+
+if ( b>max ) max = b;
+if ( c>max ) max = c;
+
+Console.Write("Максимальное число= ");
+Console.WriteLine(max);
