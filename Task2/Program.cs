ab810214779fd862f944ece77d7bdc639bd9b27f
den_minus <den.minus@mail.ru>
2023-01-24 18:39:48 +0300

    Определение является ли одно число квадратом другого

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число большее число, не меньше единицы ");
+double bignumber = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число меньшее число ");
+double smallnumber = Convert.ToDouble(Console.ReadLine());
+double n=bignumber/smallnumber;
+if (smallnumber==n) 
+{
+    Console.WriteLine("Большее число является квадратом меньшего");
+}
+else 
+{
+    Console.WriteLine("Большее число не является  квадратом меньшего");
+
+
+}
