b2f5c1ce457c03c2b3c3351c108cb8f04e9f1ee3
den_minus <den.minus@mail.ru>
2023-01-25 12:33:52 +0300

    выяснение кратности

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите делимое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите делитель ");
+int b = Convert.ToInt32(Console.ReadLine());
+int n=a%b;
+
+if (n==0)
+{
+    Console.Write("Делимое число кратно делителю");
+}
+else
+{
+    Console.Write($"Делимое число не кратно делителю. Остаток {n}");
+}
