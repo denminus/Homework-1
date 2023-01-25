e1ad1361a64dff6c1be23b28d6704b71ed68d650
den_minus <den.minus@mail.ru>
2023-01-25 12:20:04 +0300

    Удаление второй цифры трёхзначного числа

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Без второй цифры: ");
+
+int n1=n/100;
+int n2=n%10;
+Console.WriteLine($"{n1}_{n2}");
