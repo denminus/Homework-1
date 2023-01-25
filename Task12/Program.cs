d1d80d41f0f73142c2c3a413f1418ac900ac3dd0
den_minus <den.minus@mail.ru>
2023-01-25 13:03:34 +0300

    поиск третьей цифры , сообщение об отсутствии

---
+++ Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>=100)
+{
+    Console.Write("Последней цифрой вашего числа является  ");
+    Console.WriteLine(n%10); 
+
+}
+else {
+    Console.WriteLine("Третьей цифры нет");
+}
